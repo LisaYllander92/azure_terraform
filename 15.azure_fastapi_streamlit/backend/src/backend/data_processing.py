77f25e4431acfb637342a6475c728221aac2de37
LisaYllander92 <Lisaylander92@gmail.com>
2026-08-25 16:47:42 +0200

    code along azure-fastapi-streamlit

---
+++ 15.azure_fastapi_streamlit/backend/src/backend/data_processing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from backend.constants import DATA_PATH
+
+df = pd.read_csv(DATA_PATH / "Pokemon.csv")
+df["Type 2"] = df["Type 2"].fillna("missing")  # To fill NaN:s in column Type 2
+
+# Count pokemons per type (Type 1 and Type 2)
+number_per_type = (
+    pd.concat([df["Type 1"].value_counts(), df["Type 2"].value_counts()])
+    .groupby(level=0)
+    .sum().sort_values(ascending=False)
+    .drop("missing")
+)
+
+
+def filtered_types(poke_type):
+    poke_type = poke_type.capitalize().strip()
+    return df.query("`Type 1` == @poke_type or `Type 2` == @poke_type")
